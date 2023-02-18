9b0b84c546a495d073cb7a6c9664af0a56077d19
Attawut Narfkulpat <earmla.n@hotmail.co.uk>
2023-02-18 15:16:27 +0700

    update_web_18022023

--- js/custom.js
+++ js/custom.js
@@ -1,108 +1,102 @@
-// Custom Script
-// Developed by: Samson.Onna
-var customScripts = {
-    profile: function () {
-        // portfolio
-        if ($('.isotopeWrapper').length) {
-            var $container = $('.isotopeWrapper');
-            var $resize = $('.isotopeWrapper').attr('id');
-            // initialize isotope
-            $container.isotope({
-                itemSelector: '.isotopeItem',
-                resizable: false, // disable normal resizing
-                masonry: {
-                    columnWidth: $container.width() / $resize
-                }
-            });
-            $("a[href='#top']").click(function () {
-                $("html, body").animate({ scrollTop: 0 }, "slow");
-                return false;
-            });
-            $('.navbar-inverse').on('click', 'li a', function () {
-                $('.navbar-inverse .in').addClass('collapse').removeClass('in').css('height', '1px');
-            });
-            $('#filter a').click(function () {
-                $('#filter a').removeClass('current');
-                $(this).addClass('current');
-                var selector = $(this).attr('data-filter');
-                $container.isotope({
-                    filter: selector,
-                    animationOptions: {
-                        duration: 1000,
-                        easing: 'easeOutQuart',
-                        queue: false
-                    }
-                });
-                return false;
-            });
-            $(window).smartresize(function () {
-                $container.isotope({
-                    // update columnWidth to a percentage of container width
-                    masonry: {
-                        columnWidth: $container.width() / $resize
-                    }
-                });
-            });
-        }
-    },
-    fancybox: function () {
-        // fancybox
-        $(".fancybox").fancybox();
-    },
-    onePageNav: function () {
+$(document).on('click','.navbar-collapse.in',function(e) {
+               if( $(e.target).is('a') ) {
+               $(this).collapse('hide');
+               }
+               });
 
-        $('#mainNav').onePageNav({
-            currentClass: 'active',
-            changeHash: false,
-            scrollSpeed: 950,
-            scrollThreshold: 0.2,
-            filter: '',
-            easing: 'swing',
-            begin: function () {
-                //I get fired when the animation is starting
-            },
-            end: function () {
-                //I get fired when the animation is ending
-            },
-            scrollChange: function ($currentListItem) {
-                //I get fired when you enter a section and I pass the list item of the section
-            }
-        });
-    },
-    slider: function () {
-        $('#da-slider').cslider({
-            autoplay: true,
-            bgincrement: 0
-        });
-    },
-    owlSlider: function () {
-        var owl = $("#owl-demo");
-        owl.owlCarousel();
-        // Custom Navigation Events
-        $(".next").click(function () {
-            owl.trigger('owl.next');
-        })
-        $(".prev").click(function () {
-            owl.trigger('owl.prev');
-        })
-    },
-    bannerHeight: function () {
-        var bHeight = $(".banner-container").height();
-        $('#da-slider').height(bHeight);
-        $(window).resize(function () {
-            var bHeight = $(".banner-container").height();
-            $('#da-slider').height(bHeight);
-        });
-    },
-    init: function () {
-        customScripts.onePageNav();
-        customScripts.profile();
-        customScripts.fancybox();
-        customScripts.slider();
-        customScripts.owlSlider();
-        customScripts.bannerHeight();
+$(function () {
+  $('#bs-example-navbar-collapse-1').on('shown.bs.collapse', function(e) {
+                                        $('#my_dropdown').dropdown('toggle', 'open').hide();
+                                        console.log('shown:', e);
+                                        });
+  });
+
+
+function spiderjam(mym, myd){
+    document.write("<a href=mailto:"	+ mym + "&#64;" + myd + ">" + mym + "&#64;" + myd + "</a>");
+}
+
+function firefox() {
+    var browser=navigator.userAgent.toLowerCase();
+    if(browser.indexOf('firefox') > -1) {
+        $('#bs-example-navbar-collapse-2').toggle();
     }
 }
-$('document').ready(function () {
-    customScripts.init();
-});
+
+function reset_menus() {
+    sizing($(window).width());
+}
+
+// Take action if the request on URI has internal link '#'
+function internal_link() {
+    var urlArray = window.location.href.split('#');
+    if (urlArray.length == 2 && urlArray[1] != "") {
+        switch (urlArray[1]) {
+            case "intro":        $('#tab-1-content').show(); break;
+            case "news":         $('#tab-2-content').show(); break;
+            case "publications": $('#tab-3-content').show(); break;
+            case "courses":      $('#tab-4-content').show(); break;
+            case "awards":       $('#tab-5-content').show(); break;
+            case "service":      $('#tab-6-content').show(); break;
+            case "contact":      $('#tab-9-content').show(); break;
+            default: break;
+        }
+    }
+}
+
+function sizing(windowWidth) {
+    //alert(windowWidth);
+    if(windowWidth < 1024){ // desktop size
+        $('.twittertop').hide();
+        $('.twitterlow').show();
+    } else {
+        $('.twittertop').show();
+        $('.twitterlow').hide();
+    }
+
+    if(windowWidth <= 480){ // ipad:768, Nexus10:800, 480
+        $('.allshow').hide(); //
+        $('.noshow').show();
+        $('.expandshow').show();
+        $('.collapseshow').hide();
+    } else {
+        $('.allshow').show();
+        $('.noshow').hide();
+        $('.expandshow').hide();
+        $('.collapseshow').hide();
+    }
+}
+
+jQuery(document).ready(function($) {
+                       var windowWidth = $(window).width();
+                       $(window).resize(function(){
+                                        // Check window width has actually changed and it's not just iOS triggering a resize event on scroll
+                                        if ($(window).width() != windowWidth) {
+                                        windowWidth = $(window).width();
+                                        sizing(windowWidth);
+                                        }
+                                        });
+                       sizing(windowWidth);
+                       internal_link();
+                       });
+
+
+// Google Site Search | Search box
+(function() {
+ var cx = '012777421281883581498:r3xp4rmwevw';
+ var gcse = document.createElement('script');
+ gcse.type = 'text/javascript';
+ gcse.async = true;
+ gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+ '//cse.google.com/cse.js?cx=' + cx;
+ var s = document.getElementsByTagName('script')[0];
+ s.parentNode.insertBefore(gcse, s);
+ })();
+
+// Google Analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+ })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-70658068-1', 'auto');
+ga('send', 'pageview');
